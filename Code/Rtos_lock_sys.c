9ba23a96eb7238dc631433762f9bd30724957a42
Aditya Patil <146908525+adityaatil@users.noreply.github.com>
2026-09-18 16:26:10 +0530

    Add Keil RTX smart door lock firmware for LPC1768

--- Code/Rtos_lock_sys.c
+++ Code/Rtos_lock_sys.c
@@ -1,53 +1,17 @@
 #include <LPC17xx.h>
 #include <RTL.h>
+#include <string.h>
 #include "UART0.h"
 #include "Lcd.h"
 
-#define LED_PIN       (1 << 18)       /* P1.18 */
+#define LED_PIN       (1 << 18)      /* P1.18 */
 #define MAX_ATTEMPTS  3
 #define PASSWORD      "1234"
 
-enum { LED_OFF, ACCESS_GRANTED, SYSTEM_LOCKED };
-volatile unsigned char lock_state = LED_OFF;
 volatile unsigned char attempts = 0;
-
-static char entered[5];
-static unsigned char index_pos = 0;
-static unsigned char forgot_pos = 0;
-static const char reset_word[] = "forgot";
-
-static void led_delay(unsigned int ticks)
-{
-    os_dly_wait(ticks);
-}
-
-__task void LedTask(void)
-{
-    unsigned char i;
-
-    while (1) {
-        if (lock_state == SYSTEM_LOCKED) {
-            LPC_GPIO1->FIOSET = LED_PIN;
-            led_delay(50);
-            LPC_GPIO1->FIOCLR = LED_PIN;
-            led_delay(50);
-        } else if (lock_state == ACCESS_GRANTED) {
-            for (i = 0; i < 3 && lock_state == ACCESS_GRANTED; i++) {
-                LPC_GPIO1->FIOSET = LED_PIN;
-                led_delay(30);
-                LPC_GPIO1->FIOCLR = LED_PIN;
-                led_delay(30);
-            }
-            if (lock_state == ACCESS_GRANTED) {
-                LPC_GPIO1->FIOSET = LED_PIN;
-                os_tsk_delete_self();
-            }
-        } else {
-            LPC_GPIO1->FIOCLR = LED_PIN;
-            led_delay(1);
-        }
-    }
-}
+char pass_input[5];
+unsigned char pass_idx = 0;
+unsigned char forgot_idx = 0;
 
 static void show_prompt(void)
 {
@@ -57,28 +21,53 @@ static void show_prompt(void)
     lcd_com();
 }
 
-static void reset_input(void)
+static void reset_password_state(void)
 {
-    index_pos = 0;
-    forgot_pos = 0;
+    pass_idx = 0;
+    forgot_idx = 0;
     show_prompt();
 }
 
-__task void PasswordTask(void)
+__task void LED_Task(void)
 {
+    while (1)
+    {
+        if (attempts >= MAX_ATTEMPTS)
+        {
+            LPC_GPIO1->FIOSET = LED_PIN;
+            os_dly_wait(50);
+            LPC_GPIO1->FIOCLR = LED_PIN;
+            os_dly_wait(50);
+        }
+        else
+        {
+            LPC_GPIO1->FIOCLR = LED_PIN;
+            os_dly_wait(1);
+        }
+    }
+}
+
+__task void DoorLock_Task(void)
+{
+    const char forgot_word[] = "forgot";
     unsigned char ch;
 
     show_prompt();
 
-    while (1) {
-        if (attempts >= MAX_ATTEMPTS) {
-            lock_state = SYSTEM_LOCKED;
+    while (1)
+    {
+        if (attempts >= MAX_ATTEMPTS)
+        {
             clr_disp();
             lcd_puts((unsigned char *)"SYSTEM LOCKED");
-            os_tsk_delete_self();
+            while (1)
+            {
+                os_dly_wait(1);
+            }
         }
 
-        if (!rx0_flag) {
+        if (!rx0_flag)
+        {
             os_dly_wait(1);
             continue;
         }
@@ -86,46 +75,53 @@ __task void PasswordTask(void)
         ch = recv_data;
         rx0_flag = 0;
 
-        if (ch >= 'A' && ch <= 'Z') {
-            ch += 'a' - 'A';
-        }
+        if (ch >= 'A' && ch <= 'Z')
+            ch += 32;    /* convert to lowercase */
 
-        /* Type "forgot" to clear the failed-attempt counter. */
-        if (ch == reset_word[forgot_pos]) {
-            if (++forgot_pos == 6) {
+        /* forgot reset */
+        if (ch == forgot_word[forgot_idx])
+        {
+            forgot_idx++;
+            if (forgot_idx == 6)
+            {
                 attempts = 0;
-                lock_state = LED_OFF;
+                forgot_idx = 0;
                 clr_disp();
                 lcd_puts((unsigned char *)"RESET PASSWORD");
                 os_dly_wait(200);
-                reset_input();
+                reset_password_state();
             }
             continue;
         }
-        forgot_pos = 0;
+        forgot_idx = 0;
 
-        if (ch < '0' || ch > '9') {
+        /* accept only digits */
+        if (ch < '0' || ch > '9')
             continue;
-        }
 
-        entered[index_pos++] = ch;
+        pass_input[pass_idx++] = ch;
         temp1 = '*';
         lcd_data();
 
-        if (index_pos == 4) {
-            entered[4] = '\0';
+        if (pass_idx == 4)
+        {
+            pass_input[4] = '\0';
             clr_disp();
 
-            if (strcmp(entered, PASSWORD) == 0) {
+            if (strcmp(pass_input, PASSWORD) == 0)
+            {
                 lcd_puts((unsigned char *)"ACCESS GRANTED");
-                lock_state = ACCESS_GRANTED;
-                os_tsk_delete_self();
+                LPC_GPIO1->FIOSET = LED_PIN;
+                while (1)
+                {
+                    os_dly_wait(1);
+                }
             }
 
             lcd_puts((unsigned char *)"WRONG PASSWORD");
             attempts++;
             os_dly_wait(150);
-            reset_input();
+            reset_password_state();
         }
     }
 }
@@ -137,13 +133,15 @@ int main(void)
 
     lcd_init();
     UART0_Init();
+
     LPC_GPIO1->FIODIR |= LED_PIN;
     LPC_GPIO1->FIOCLR = LED_PIN;
 
-    os_tsk_create(LedTask, 1);
-    os_tsk_create(PasswordTask, 1);
-    os_sys_init(PasswordTask);
+    os_tsk_create(LED_Task, 1);
+    os_tsk_create(DoorLock_Task, 2);
+    os_sys_init_user();
 
-    while (1) {
+    while (1)
+    {
     }
 }
